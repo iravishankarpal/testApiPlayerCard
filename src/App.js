3a44ee17aa99a1406b56ac016581830aca3e3de6
iravishankarpal <iravishankarpal@gmail.com>
2022-04-13 09:42:09 +0530

    test completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import { React, useEffect, useState } from "react";
+import "./App.css";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+// let c = [];
+// let b = [];
 function App() {
+  
+  const [playerList, setPlayerList] = useState([]);
+  const [searchTerm,setSearchTerm] = useState("");
+  async function fetchData() {
+    try {
+      var a = await  fetch("https://api.npoint.io/20c1afef1661881ddc9c");
+      var b = await a.json();
+        // playerList = b.playerList;
+       setPlayerList(  b.playerList);
+      
+      } catch (error ) {
+        console.log("network" ,error);
+      }
+    }
+
+  //   function handler(event) {
+      
+  //     console.log(event.target.value)
+  //     let searchValue = event.target.value;
+  //     console.log("first",playerList);
+  //     let filteredCharacters = playerList.filter((e) => {
+  //       return (
+  //          e.TName.toLowerCase().includes(searchValue)  ||
+  //         e.PFName.toLowerCase().includes(searchValue)
+  //         );
+          
+          
+  //       });
+  //       console.log("second",playerList);
+  //       setPlayerList(filteredCharacters);
+  // }
+
+
+  useEffect(() => {
+         fetchData();
+     },[]);
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <div className="container-fluid">
+
+    
+      <div className="container">
+        <div className="row">
+          <div className="hederr">
+            <h1>Top 50 player's</h1>
+          </div>
+          <input type="text" placeholder="search player here" id="searchBox"  onKeyUp={(e) => setSearchTerm(e.target.value)}/>
+        {
+          // function display(playerList){
+
+          // }
+          playerList.filter((val)=>{
+            return val.TName.toLowerCase().includes(searchTerm.toLowerCase()) || val.PFName.toLowerCase().includes(searchTerm.toLowerCase())
+          }).reverse().map((player) => {
+            // console.log(player.PFName);
+           return (
+           <div className="col-md-4 col-sm-6 col-lg-3  col-">
+           <div className="card">
+            <img src={`./player-images/${player.Id}.jpg`} className="card-img-top" alt="player img" />
+            <div className="card-body">
+              <h5 className="card-title">{player.PFName}</h5>
+              <h6>{player.SkillDesc}</h6>
+              <h6>{player.Value} $</h6>
+              <h6>{player.TName}</h6>
+              <h6>{player.UpComingMatchesList[0].CCode} Vs {player.UpComingMatchesList[0].VsCCode} : {player.UpComingMatchesList[0].MDate}</h6>
+        
+                         </div>
+           </div>
+            </div>
+          
+           )} 
+
+           
+        )
+        }
+          
+        </div>
+      </div>
+      </div>
+    </>
+  
   );
 }
 
